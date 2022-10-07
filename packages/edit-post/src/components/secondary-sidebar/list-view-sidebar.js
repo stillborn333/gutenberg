50be58b40ff0b8851b9475463f5528a3aa197b78
Jorge <jorge.costa@developer.pt>
2022-10-07 21:05:55 +0100

    Move document information and outline to list view panel

--- packages/edit-post/src/components/secondary-sidebar/list-view-sidebar.js
+++ packages/edit-post/src/components/secondary-sidebar/list-view-sidebar.js
@@ -1,8 +1,13 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { __experimentalListView as ListView } from '@wordpress/block-editor';
-import { Button } from '@wordpress/components';
+import { Button, __experimentalText as Text } from '@wordpress/components';
 import {
 	useFocusOnMount,
 	useFocusReturn,
@@ -13,6 +18,13 @@ import { useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { closeSmall } from '@wordpress/icons';
 import { ESCAPE } from '@wordpress/keycodes';
+import { useState } from '@wordpress/element';
+import {
+	DocumentOutline,
+	WordCount,
+	TimeToRead,
+	CharacterCount,
+} from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -35,6 +47,8 @@ export default function ListViewSidebar() {
 	const instanceId = useInstanceId( ListViewSidebar );
 	const labelId = `edit-post-editor__list-view-panel-label-${ instanceId }`;
 
+	const [ tab, setTab ] = useState( 'list-view' );
+
 	return (
 		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
 		<div
@@ -43,10 +57,49 @@ export default function ListViewSidebar() {
 			onKeyDown={ closeOnEscape }
 		>
 			<div
-				className="edit-post-editor__list-view-panel-header"
+				className="edit-post-editor__list-view-panel-header components-panel__header edit-post-sidebar__panel-tabs"
 				ref={ headerFocusReturnRef }
 			>
-				<strong id={ labelId }>{ __( 'List View' ) }</strong>
+				<ul>
+					<li>
+						<Button
+							onClick={ () => {
+								setTab( 'list-view' );
+							} }
+							className={ classnames(
+								'edit-post-sidebar__panel-tab',
+								{ 'is-active': tab === 'list-view' }
+							) }
+							data-label={ __( 'List View' ) }
+							aria-label={
+								tab === 'list-view'
+									? __( 'List View (selected)' )
+									: __( 'List View' )
+							}
+						>
+							{ __( 'List View' ) }
+						</Button>
+					</li>
+					<li>
+						<Button
+							onClick={ () => {
+								setTab( 'outline' );
+							} }
+							className={ classnames(
+								'edit-post-sidebar__panel-tab',
+								{ 'is-active': tab === 'outline' }
+							) }
+							data-label={ __( 'Outline' ) }
+							aria-label={
+								tab === 'outline'
+									? __( 'Outline (selected)' )
+									: __( 'Outline' )
+							}
+						>
+							{ __( 'Outline' ) }
+						</Button>
+					</li>
+				</ul>
 				<Button
 					icon={ closeSmall }
 					label={ __( 'Close List View Sidebar' ) }
@@ -60,7 +113,24 @@ export default function ListViewSidebar() {
 					focusOnMountRef,
 				] ) }
 			>
-				<ListView />
+				{ tab === 'list-view' && <ListView /> }
+				{ tab === 'outline' && <DocumentOutline /> }
+				<div className="edit-post-editor__list-view-overview">
+					<div>
+						<Text>{ __( 'Characters:' ) }</Text>
+						<Text>
+							<CharacterCount />
+						</Text>
+					</div>
+					<div>
+						<Text>{ __( 'Words:' ) }</Text>
+						<WordCount />
+					</div>
+					<div>
+						<Text>{ __( 'Time to read:' ) }</Text>
+						<TimeToRead />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
